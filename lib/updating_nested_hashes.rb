a7aca89befb39e67073f0b5af4de6a102d20524d
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-17 00:54:40 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -45,12 +45,16 @@ def
 # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
 # Code your solution here:
 
-epic_tragedy[:montague]
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
+# After your code, the following code will return the altered hash. 
+# This method must only return the modified epic_tragedy hash
 
-# After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
   epic_tragedy
+
+
 end
 
 
@@ -63,34 +67,56 @@ end
 def 
   
   add_characters
-   epic_tragedy = {
-      :montague => {
+  
+  
+   epic_tragedy = 
+   {
+     
+     
+      :montague =>
+      {
+        
          :patriarch => {name: "Lord Montague", age: "53"},
          :matriarch => {name: "Lady Montague", age: "54"},
+         
          :hero => {name: "Romeo", age: "15", status: "alive"},
-         :hero_friends => [
+         :hero_friends => 
+         [
             {name: "Benvolio", age: "17", attitude: "worried"},
             {name: "Mercutio", age: "18", attitude: "hot-headed"}
          ]
+         
       },
-      :capulet => {
+      
+      :capulet => 
+      {
+        
          :patriarch => {name: "Lord Capulet", age: "50"},
          :matriarch => {name: "Lady Capulet", age: "51"},
+         
          :heroine => {name: "Juliet", age: "15", status: "alive"},
-         :heroine_friends => [
-         {name: "Steven", age: "30", attitude: "confused"},
-         {name: "Nurse", age: "44", attitude: "worried"}
+         :heroine_friends => 
+         [
+            {name: "Steven", age: "30", attitude: "confused"},
+            {name: "Nurse", age: "44", attitude: "worried"}
          ]
+         
       }
+      
+      
    }
+
 
    # Write code below that does the following programmatically:
    #
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
+   
    # - Add two hashes to this array, both with a :name key.
    #   - The first hash should have the :name key set to "Prince Escalus" 
    #   - The second hash should have the :name key set to "Apothecary" 
+   
    # Code your solution here:
+
 
 
 
@@ -100,6 +126,7 @@ def
    # After your code, the following line will return the altered hash
    
    epic_tragedy
+   
    
 end
 
